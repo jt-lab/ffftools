5d0768333ea8e539254f92ccfc37a9be97f8255c
jt-lab <jan.tuennermann@uni-marburg.de>
2025-02-09 22:12:18 +0100

    ...

--- src/ffftools/__init__.py
+++ src/ffftools/__init__.py
@@ -1,3 +1,9 @@
+__version__ = "0.0.2"
+
 from .computers import *
+from .algorithms.shp_dp import *
+from .algorithms.shp_sa import *
+from .algorithms.sequence import *
 from .utils import *
 from .data import *
+from .plots import *
